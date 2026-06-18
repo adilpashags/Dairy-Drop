825ec83fa9485fc1d74b661f749e243de7eaa44b
adil-a11y <adilpashags@gmail.com>
2026-06-18 13:09:16 +0530

    Use static product images

--- app/models.py
+++ app/models.py
@@ -52,14 +52,15 @@ CATEGORY_CHOICES = (
 )
 
 class Product(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
     selling_price = models.FloatField()
     discounted_price = models.FloatField()
     description = models.TextField()
-    composition = models.TextField(default='')
-    prodapp = models.TextField(default='')
-    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
-    product_image = models.ImageField(upload_to='product')
+    composition = models.TextField()
+    prodapp = models.TextField()
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+
+    product_image = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
